98e5a5ee00c30d06c7bbc70dee66434e17baa58e
Madona Syombua <syombuamadona@gmail.com>
2018-09-11 10:14:12 -0500

    Update AddItemActivity.java

--- app/src/main/java/com/madonasyombua/roomexample/activities/AddItemActivity.java
+++ app/src/main/java/com/madonasyombua/roomexample/activities/AddItemActivity.java
@@ -35,7 +35,7 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
         ButterKnife.bind(this);
 
-        //avoid creating empty rows and columns.
+        //avoid null entry.
         itemButton.setOnClickListener(v -> {
             Intent intentReply = new Intent();
             if (TextUtils.isEmpty(itemOne.getText()) ||
